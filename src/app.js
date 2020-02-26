54bd4ccc3692deeda101b565612336f07ada3645
Guilhermo Masid <Guilhermo@MacBook-Air-de-Guilhermo.local>
2020-02-26 15:37:10 -0300

    tratamento-de-excecoes-erros-no-async

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
 import routes from './routes';
+import sentryConfig from './config/sentry';
 
 import './database';
 
@@ -8,11 +12,15 @@ class App {
   constructor() {
     this.app = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHadler();
   }
 
   middlewares() {
+    this.app.use(Sentry.Handlers.requestHandler());
     this.app.use(express.json());
     this.app.use(
       '/files',
@@ -22,6 +30,15 @@ class App {
 
   routes() {
     this.app.use(routes);
+    this.app.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHadler() {
+    this.app.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://fbc76dae76ac4b63843a224ba8310a11@sentry.io/2989693',
+};
