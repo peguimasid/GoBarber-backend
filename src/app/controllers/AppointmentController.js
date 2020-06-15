56124f2613353a61f3fef73508fd218682226b36
Guilhermo Masid <Guilhermo@MacBook-Air-de-Guilhermo.local>
2020-06-15 20:03:55 -0300

    Aula 01 - Abstraindo validacoes

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -38,15 +38,6 @@ class AppointmentController {
   }
 
   async store(req, res) {
-    const schema = Yup.object().shape({
-      provider_id: Yup.number().required(),
-      date: Yup.date().required(),
-    });
-
-    if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validation fails' });
-    }
-
     const { provider_id, date } = req.body;
 
     // check id provider_id is a provider
