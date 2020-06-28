c95ba2c651d35512a1e246ffb90daee293b3fa21
Guilhermo Masid <Guilhermo@MacBook-Air-de-Guilhermo.local>
2020-06-28 16:55:55 -0300

    Aula 03 - Utilizando Rate Limit

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ import express from 'express';
 import path from 'path';
 import cors from 'cors';
 import helmet from 'helmet';
+import redis from 'redis';
+import RateLimit from 'express-rate-limit';
+import RateLimitRedis from 'rate-limit-redis';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import 'express-async-errors';
@@ -28,10 +31,26 @@ class App {
     this.app.use(helmet());
     this.app.use(cors());
     this.app.use(express.json());
+
     this.app.use(
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
     );
+
+    if (process.env.NODE_ENV !== 'development') {
+      this.app.use(
+        new RateLimit({
+          store: new RateLimitRedis({
+            client: redis.createClient({
+              host: process.env.REDIS_HOST,
+              port: process.env.REDIS_PORT,
+            }),
+          }),
+          windowMs: 1000 * 60 * 15,
+          max: 100,
+        })
+      );
+    }
   }
 
   routes() {
